66b6564e2a8e727e090dac55c8697af54cad4e91
Dolmatov <ADolmatov@omsk.luxoft.com>
2017-12-15 11:54:39 +0600

    fix some style on recent and category pages

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -9,34 +9,11 @@ class Book extends Component {
     super(props);
 
     this.state = {
-      bookDescLines: 0
     }
-
-    this.bookDescResize = this.bookDescResize.bind(this);
-  }
-
-  bookDescResize(book) {
-    if(!book.desc) return;
-
-    let bookBlock = this.refs.book;
-
-    let columnHeight = Math.ceil((bookBlock.clientWidth - book.desc.offsetTop + 49) / 38);
-
-    this.setState({bookDescLines: columnHeight});
-  }
-
-  componentDidMount() {
-    window.addEventListener("resize", this.bookDescResize.bind(this.bookDescResize, this.refs));
-    this.bookDescResize(this.refs);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.bookDescResize);
   }
 
   render() {
     const { subClass, author, cover, title, description, reviews } = this.props;
-    const { bookDescLines } = this.state;
 
     return (
       <article ref="book" className={subClass ? "book " + subClass : "book"}>
@@ -44,7 +21,7 @@ class Book extends Component {
         <section className="book__info">
           <Link to="/book-description" className="book__title">{title}</Link>
           <div className="book__author">{author}</div>
-          <div className="book__desc" ref="desc" style={{WebkitLineClamp: bookDescLines}}>{description}</div>
+          <div className="book__desc" ref="desc">{description}</div>
           <div className="reviews book__reviews">{reviews}</div>
         </section>
       </article>
