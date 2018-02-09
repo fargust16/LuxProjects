f4e155eb67c5f3f4591884d683aa6034891e4cbc
Dolmatov <ADolmatov@omsk.luxoft.com>
2018-02-09 19:19:05 +0600

    create all books query from database

--- server/index.js
+++ server/index.js
@@ -30,22 +30,22 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 app.get('/books', (req, res) => {
     //res.send(books);
 
-    let struct = [
-        'books.id, books.title, genres.genre, isbn, release_date as releaseDate, text, cover, topics, array_to_json(array_agg(row_to_json(reviews))) as reviews, users.username as uploadBy',
-        'books, genres, users, reviews',
-        'genres.id = books.fk_genre AND users.id = books.fk_uploaded_by AND books.id = reviews.fk_book',
-        'books.id, genres.id, users.username'
-    ];
+    let struct = {
+        columns: ['b.book_id', 'b.title', 'g.genre', 'b.isbn', 'b.release_date', 'b.description', 'b.text', 'b.cover',
+            'b.topics']
+    };
 
-    /*db.query('SELECT $1:value FROM $2:value ' +
-        'WHERE $3:value GROUP BY $4:value', struct)*/
-
-    db.query('SELECT ${columns:name}, array_to_json(array_agg(row_to_json(reviews))) as reviews FROM ${tables:name} WHERE genre_id = fk_genre AND user_id = fk_uploaded_by AND book_id = fk_book GROUP BY ${group:name}', {
-        columns: ['book_id', 'title', 'genre', 'isbn', 'release_date', 'text', 'cover',
-            'topics', 'username'],
-        tables: ['books', 'users', 'genres', 'reviews'],
-        group: ['book_id', 'genre_id', 'username']
-    })
+    db.query('SELECT bg.*, r.rev, com.com ' +
+        'FROM (SELECT b.book_id as book_id, b.title, g.genre, b.isbn, b.release_date, b.description, ' +
+        'b.text, b.cover, b.topics FROM books as b, genres as g WHERE b.genre_id = g.genre_id) bg ' +
+        ' LEFT JOIN ' +
+        '   ( SELECT book_id, array_to_json(array_agg(row_to_json(reviews))) as rev FROM  reviews ' +
+        '   GROUP BY book_id ) r ' +
+        ' ON bg.book_id = r.book_id ' +
+        ' LEFT JOIN ' +
+        '  ( SELECT book_id, array_to_json(array_agg(row_to_json(book_comments))) as com FROM book_comments ' +
+        '   GROUP BY book_id ) AS com ' +
+        ' ON bg.book_id = com.book_id', struct)
         .then(data => {
             res.send(data);
             //console.log(data);
@@ -53,6 +53,36 @@ app.get('/books', (req, res) => {
         .catch(error => {
             res.send(error);
             console.log(error);
+        })
+
+    /*db.query('SELECT ${columns:name}, array_to_json(array_agg(row_to_json(reviews))) as reviews FROM ${tables:name} ' +
+        'WHERE genre_id = fk_genre AND user_id = fk_uploaded_by GROUP BY ${group:name}' +
+        'ORDER BY fk_book', struct)
+        .then(data => {
+            res.send(data);
+            //console.log(data);
+        })
+        .catch(error => {
+            res.send(error);
+            console.log(error);
+        })*/
+});
+
+app.post('/books/add-new-book', (req, res) => {
+    const {title, author, genre, isbn, releaseDate, description, text, cover, topics, userId} = req.body;
+
+    let params = {
+        table: 'books',
+        columns: ['book_id', 'title', 'author', 'fk_genre', 'isbn', 'release_date', 'description', 'text', 'cover', 'topics', 'fk_uploaded_by'],
+        values: ['default', title, author, genre, isbn, releaseDate, description, text, cover, topics, userId]
+    };
+
+    db.query('INSERT INTO books(${columns:name}) VALUES (${values:name}) RETURNING book_id', params)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(error => {
+            res.send(error);
         })
 });
 
