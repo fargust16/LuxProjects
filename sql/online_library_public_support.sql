d81a742aedf7c9eede7ea6c362be4d5b1d4df901
Dolmatov <ADolmatov@omsk.luxoft.com>
2018-02-22 11:20:08 +0600

    add sql

---
+++ sql/online_library_public_support.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.support (id, subject, message) VALUES (1, 'Проблемы с добавлением книги', 'Вы хоть видели, как ваше приложение выглядит в Edge?! Вообще ничего не работает! Дизлайн, отписка');
+INSERT INTO public.support (id, subject, message) VALUES (2, 'Второе моё сообщение', 'Ну что на этот раз добавится в поддержку');
+INSERT INTO public.support (id, subject, message) VALUES (3, 'Третье сообщение', 'Второе доставлено удачно, проверка на попап');
+INSERT INTO public.support (id, subject, message) VALUES (4, 'Тратата', 'Афганистан');
+INSERT INTO public.support (id, subject, message) VALUES (5, 'Абракадабра', 'Тестируем попанп');
+INSERT INTO public.support (id, subject, message) VALUES (6, 'Который тут главный', 'Эйй, фииии, кто тут за всё отвечает?');
+INSERT INTO public.support (id, subject, message) VALUES (7, 'Проверка на ресет после сабмита', 'Первый первый я второй, как слышно!:?');
